c1aef9528787aa8e1a086f8273263646c6f40b91
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-07-05 21:43:17 +0200

    updated bspwm config and polybar themes

--- config/bspwm/polybar/scripts/notification-github.sh
+++ config/bspwm/polybar/scripts/notification-github.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-USER="userName"
+#
+# Copyright (C) 2022 Mahmoud Mohamed (Ozil)  <https://github.com/mmsaeed509>
+# LICENSE © GNU-GPL3
+#
+
+USER="yourUserName"
 # You can get your Personal access tokens from here : https://github.com/settings/tokens #
-TOKEN=""
+TOKEN="yourToken"
 
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
-if [ "$notifications" -gt 0 ]; then
-    echo "%{T3} $notifications"
-else
-    echo "%{T3} 0"
-fi
+echo "%{T3} $notifications"
+
