6be17d07b4adbb5446ee5fc2a2d22bb6ebdfcfe2
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-08-01 10:22:45 +0200

    updated bspwm

--- config/bspwm/polybar/scripts/notification-github.sh
+++ config/bspwm/polybar/scripts/notification-github.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-USER="yourUserName"
+USER="userName"
 # You can get your Personal access tokens from here : https://github.com/settings/tokens #
-TOKEN="yourPersonalAccessTokens"
+TOKEN="personalAccessTokens"
 
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
