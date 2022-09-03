2ab35eb1c9a1cc03e37db2826c99309555f047a0
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-09-03 14:50:47 +0200

    added Tokyo City Theme

--- config/bspwm/polybar/scripts/notification-github.sh
+++ config/bspwm/polybar/scripts/notification-github.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-USER="mmsaeed509"
+USER="userName"
 # You can get your Personal access tokens from here : https://github.com/settings/tokens #
-TOKEN="personalAccessTokens"
+TOKEN="yourPersonalAccessTokens"
 
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
 echo "%{T3} $notifications"
-
