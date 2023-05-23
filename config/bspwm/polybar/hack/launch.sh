05a7cb549b84acf4a9429d457701f80caf8a6103
Mahmoud Mohamed (00xWolf - mmsaeed509) <mmsaeed509@gmail.com>
2023-05-23 22:46:52 +0300

    updated src code

--- config/bspwm/polybar/hack/launch.sh
+++ config/bspwm/polybar/hack/launch.sh
@@ -1,30 +1,23 @@
 #!/usr/bin/env bash
 
-##########################################################################################################
-#																										 
-#    Copyright © 2022 To Cyb3rTh1eveZ																	 
-#																										 
-#     ██████╗██╗   ██╗██████╗ ██████╗ ██████╗ ████████╗██╗  ██╗ ██╗███████╗██╗   ██╗███████╗███████╗	 
-#    ██╔════╝╚██╗ ██╔╝██╔══██╗╚════██╗██╔══██╗╚══██╔══╝██║  ██║███║██╔════╝██║   ██║██╔════╝╚══███╔╝     
-#    ██║      ╚████╔╝ ██████╔╝ █████╔╝██████╔╝   ██║   ███████║╚██║█████╗  ██║   ██║█████╗    ███╔╝      
-#    ██║       ╚██╔╝  ██╔══██╗ ╚═══██╗██╔══██╗   ██║   ██╔══██║ ██║██╔══╝  ╚██╗ ██╔╝██╔══╝   ███╔╝       
-#    ╚██████╗   ██║   ██████╔╝██████╔╝██║  ██║   ██║   ██║  ██║ ██║███████╗ ╚████╔╝ ███████╗███████╗     
-#     ╚═════╝   ╚═╝   ╚═════╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝ ╚═╝╚══════╝  ╚═══╝  ╚══════╝╚══════╝     
-#																										 
-#																										 
-#																										 
-#    Copyright (C) Mahmoud Mohamed (00xWolf)  <https://github.com/mmsaeed509>								 
-#    LICENSE © GNU-GPL3																					 
-#																										 
-##########################################################################################################
+#####################################
+#                                   #
+#  @author      : 00xWolf           #
+#    GitHub    : @mmsaeed509       #
+#    Developer : Mahmoud Mohamed   #
+#  﫥  Copyright : Exodia OS         #
+#                                   #
+#####################################
 
 # Files and Directories #
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 POLYBAR_DIR="$HOME/.config/bspwm/polybar"
 SFILE="$POLYBAR_DIR/system"
+CONFIG_FILE="$HOME/.config/bspwm/exodia.conf"
+MONITORS=$(grep -Po 'multi-bar-monitors\s*=\s*\K.*' ${CONFIG_FILE})
 
 # Get system variable values for various modules #
-get_values() {
+GET_VALUES() {
 
 	CARD=$(light -L | grep 'backlight' | head -n1 | cut -d'/' -f3)
 	BATTERY=$(upower -i `upower -e | grep 'BAT'` | grep 'native-path' | cut -d':' -f2 | tr -d '[:blank:]')
@@ -34,7 +27,7 @@ get_values() {
 }
 
 # Write values to `system` file #
-set_values() {
+SET_VALUES() {
 
 	if [[ "$ADAPTER" ]]; then
 		sed -i -e "s/adapter = .*/adapter = $ADAPTER/g" 						${SFILE}
@@ -51,8 +44,8 @@ set_values() {
 
 }
 
-# Launch Polybar with selected style #
-launch_bar() {
+# Launch Polybar with the selected style ONLY in the primary display #
+LAUNCH_SINGLE_BAR() {
 	
 	CARD=$(light -L | grep 'backlight' | head -n1 | cut -d'/' -f3)
 	INTERFACE=$(ip link | awk '/state UP/ {print $2}' | tr -d :)
@@ -61,10 +54,6 @@ launch_bar() {
 		sed -i -e 's/backlight/bna/g' "$DIR"/config
 	elif [[ "$CARD" != *"intel_"* ]]; then
 		sed -i -e 's/backlight/brightness/g' "$DIR"/config
-	fi
-
-	if [[ "$INTERFACE" == e* ]]; then
-		sed -i -e 's/network/ethernet/g' "$DIR"/config
 	fi
 	
     # close polybar #
@@ -75,12 +64,53 @@ launch_bar() {
 
     # launch polybar #
     polybar -q main -c "$DIR"/config &
+	polybar -q sec -c "$DIR"/config &
+	polybar -q third -c "$DIR"/config &
+	
+}
+
+# Launch Polybar with the selected style in all displays #
+LAUNCH_MULTI_BAR() {
+	
+	CARD=$(light -L | grep 'backlight' | head -n1 | cut -d'/' -f3)
+	INTERFACE=$(ip link | awk '/state UP/ {print $2}' | tr -d :)
+
+	if [[ -z "$CARD" ]]; then
+		sed -i -e 's/backlight/bna/g' "$DIR"/config
+	elif [[ "$CARD" != *"intel_"* ]]; then
+		sed -i -e 's/backlight/brightness/g' "$DIR"/config
+	fi
+	
+    # close polybar #
+	killall -q polybar
+
+    # Wait until closing polybar # 
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+    # launch polybar #
+	for mon in $(polybar --list-monitors | cut -d":" -f1); 
+		do
+
+			MONITOR=$mon polybar -q main -c "$DIR"/config &
+			MONITOR=$mon polybar -q sec -c "$DIR"/config &
+			MONITOR=$mon polybar -q third -c "$DIR"/config &
+
+	done
 	
 }
 
 # Execute functions #
-get_values
-set_values
+GET_VALUES
+SET_VALUES
 
 # launch polybar #
-launch_bar
+if [[ $MONITORS == "true" ]]; 
+	then
+
+    	LAUNCH_MULTI_BAR
+
+else
+
+    	LAUNCH_SINGLE_BAR
+
+fi
--- config/bspwm/polybar/scripts/notification-github.sh
+++ config/bspwm/polybar/scripts/notification-github.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-USER="username"
+USER="userName"
 # You can get your Personal access tokens from here : https://github.com/settings/tokens #
-TOKEN="accessToken"
+TOKEN="accessTokens"
 
 notifications=$(echo "user = \"$USER:$TOKEN\"" | curl -sf -K- https://api.github.com/notifications | jq ".[].unread" | grep -c true)
 
